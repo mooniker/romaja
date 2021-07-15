b9e1306a59dbb014017dc21151035ade705a4b91
James Moon <mooniker@gmail.com>
2021-07-15 01:17:26 -0400

    Add more Hangul composition tests

--- src/hangul/unicode/compose.js
+++ src/hangul/unicode/compose.js
@@ -1,12 +1,12 @@
-const { SBase, LBase, VBase, TBase, NCount, TCount } = require("./constraints");
+const { SBase, LBase, VBase, TBase, NCount, TCount } = require('./constraints')
 
 /**
  * Computes TIndex for a given TPart
  *
- * @param {integer} TPart
- * @returns {integer}
+ * @param {number} TPart
+ * @returns {number}
  */
-const computeTIndexFromTPart = (TPart = 0) => TPart - TBase;
+const computeTIndexFromTPart = (TPart = 0) => TPart - TBase
 
 /**
  * Computes a precomposed/composite syllable mapping for a given a Hangul letter (jamo) sequence
@@ -22,39 +22,39 @@ const computeTIndexFromTPart = (TPart = 0) => TPart - TBase;
  * The algorithm is described in the core spec (v. 12.1, Chapter 3), under section
  * "3.12 Conjoining Jamo Behavior" (pp. 142-151).
  *
- * @param {integer} LPart
- * @param {integer} VPart
- * @param {integer} TPart
- * @returns {integer}
+ * @param {number} LPart
+ * @param {number} VPart
+ * @param {number} TPart
+ * @returns {number}
  */
-function arithmeticPrimaryCompositeMapping(LPart, VPart, TPart) {
-  if (LPart < 0x1100 && LPart > 0x1112) {
-    throw new Error("LPart not within range U+1100..U+1112: " + LPart);
+function arithmeticPrimaryCompositeMapping (LPart, VPart, TPart) {
+  if (LPart < 0x1100 || LPart > 0x1112) {
+    throw new Error('LPart not within range U+1100..U+1112: ' + LPart)
   }
 
-  if (VPart < 0x1161 && VPart > 0x1175) {
-    throw new Error("VPart not within range U+1161..U+1175: " + VPart);
+  if (VPart < 0x1161 || VPart > 0x1175) {
+    throw new Error('VPart not within range U+1161..U+1175: ' + VPart)
   }
 
-  if (TPart && TPart < 0x11a8 && TPart > 0x11c2) {
-    throw new Error("TPart not within range U+11A8..U+11C2: " + TPart);
+  if ((TPart && TPart < 0x11a8) || TPart > 0x11c2) {
+    throw new Error('TPart not within range U+11A8..U+11C2: ' + TPart)
   }
 
-  const LIndex = LPart - LBase;
-  const VIndex = VPart - VBase;
-  const LVIndex = LIndex * NCount + VIndex * TCount;
-  const TIndex = computeTIndexFromTPart(TPart);
+  const LIndex = LPart - LBase
+  const VIndex = VPart - VBase
+  const LVIndex = LIndex * NCount + VIndex * TCount
+  const TIndex = computeTIndexFromTPart(TPart)
 
-  return SBase + LVIndex + TIndex;
+  return SBase + LVIndex + TIndex
 }
 
-function arithmeticPrimaryCompositeMappingWithLVPart(LVPart, TPart) {
-  const TIndex = computeTIndexFromTPart(TPart);
-  return LVPart + TIndex;
+function arithmeticPrimaryCompositeMappingWithLVPart (LVPart, TPart) {
+  const TIndex = computeTIndexFromTPart(TPart)
+  return LVPart + TIndex
 }
 
 module.exports = {
   computeTIndexFromTPart,
   arithmeticPrimaryCompositeMapping,
   arithmeticPrimaryCompositeMappingWithLVPart
-};
+}
--- src/hangul/unicode/compose.test.js
+++ src/hangul/unicode/compose.test.js
@@ -1,13 +1,13 @@
 const {
   computeTIndexFromTPart,
   arithmeticPrimaryCompositeMapping
-} = require("./compose");
+} = require('./compose')
 
-describe("computeTIndexFromTPart", () => {
-  test("should be tested", () => {
-    expect(computeTIndexFromTPart());
-  });
-});
+describe('computeTIndexFromTPart', () => {
+  test('should be tested', () => {
+    expect(computeTIndexFromTPart())
+  })
+})
 
 const testCases = [
   {
@@ -18,22 +18,66 @@ const testCases = [
   //   jamoCodePoints: [0x1100, 0x1162],
   //   expectedCodePoint: 0xac1c
   // }
-];
+]
 
-describe("arithmeticPrimaryCompositeMapping", () => {
+describe('arithmeticPrimaryCompositeMapping', () => {
   testCases.forEach(({ jamoCodePoints, expectedCodePoint }) => {
-    test(`should compose (${jamoCodePoints.join(", ")}) (${String.fromCodePoint(
+    test(`should compose (${jamoCodePoints.join(', ')}) (${String.fromCodePoint(
       ...jamoCodePoints
     )}) to ${expectedCodePoint} (${String.fromCodePoint(
       expectedCodePoint
     )})`, () => {
       expect(arithmeticPrimaryCompositeMapping(...jamoCodePoints)).toBe(
         expectedCodePoint
-      );
-    });
-  });
-});
+      )
+    })
+  })
 
-describe("arithmeticPrimaryCompositeMappingWithLVPart", () => {
-  test.todo("should be tested");
-});
+  describe('should throw an error if', () => {
+    describe('supplied LPart', () => {
+      test('below valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1099, 0x1171, 0x11b6)
+        }).toThrow()
+      })
+
+      test('above valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1113, 0x1171, 0x11b6)
+        }).toThrow()
+      })
+    })
+
+    describe('supplied VPart', () => {
+      test('below valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1111, 0x1160, 0x11b6)
+        }).toThrow()
+      })
+
+      test('above valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1111, 0x1176, 0x11b6)
+        }).toThrow()
+      })
+    })
+
+    describe('supplied TPart', () => {
+      test('below valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1111, 0x1171, 0x11a7)
+        }).toThrow()
+      })
+
+      test('above valid range', () => {
+        expect(() => {
+          arithmeticPrimaryCompositeMapping(0x1111, 0x1171, 0x11c3)
+        }).toThrow()
+      })
+    })
+  })
+})
+
+describe('arithmeticPrimaryCompositeMappingWithLVPart', () => {
+  test.todo('should be tested')
+})
